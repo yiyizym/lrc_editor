dbe08ae5e82cf2c63c1e77dea6029b7b95dbada4
zhuyimin <zhuyimin@meizu.com>
2018-09-07 19:11:50 +0800

    setup framework

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const webpack = require('webpack')
+
+module.exports = {
+    mode: process.env.WEBPACK_MODE || 'development',
+    // mode: 'development',
+    entry: './src/index.js',
+    resolve: {
+        alias: {
+        }
+    },
+    devServer: {
+        contentBase: './build',
+        hot: true
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            include: path.resolve(__dirname, "src"),
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        },{
+            test: /\.jsx?$/,
+            include: path.resolve(__dirname, "src"),
+            use: {
+                loader: "babel-loader"
+            }
+        }]
+    },
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'lrc_editor',
+            template: 'src/tpl/index.html'
+        }),
+        new CleanWebpackPlugin(['build']),
+        new webpack.HotModuleReplacementPlugin()
+    ]
+};
